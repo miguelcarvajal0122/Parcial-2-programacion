cf73482dd2c77fcf81e32a2aef9ebdf9b070e4e3
Neroshv <iversonhernandez2002@gmail.com>
2024-11-01 19:51:31 -0500

    anexo connection

---
+++ src/bd/connection.java
@@ -0,0 +1,20 @@
+package bd;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class connection {
+    private static Connection connection;
+
+    public static final String URL = System.getenv("");
+public static final String USER = System.getenv("");
+public static final String PASSW0RD = System.getenv("");
+
+public static connection getConnetion(){
+
+
+
+
+}
+}
